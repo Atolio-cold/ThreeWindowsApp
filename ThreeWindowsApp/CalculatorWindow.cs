8f2da02a96a7f477fe77c63f28e2dc8347780472
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-10-31 13:04:33 +0100

    Update CalculatorWindow.cs

--- ThreeWindowsApp/CalculatorWindow.cs
+++ ThreeWindowsApp/CalculatorWindow.cs
@@ -1,62 +1,75 @@
 using System;
 using System.Windows.Forms;
 
-namespace ThreeWindowsApp
+public partial class CalculatorWindow : Form
 {
-    public class CalculatorWindow : Form
+    public CalculatorWindow()
     {
-        private TextBox input1;
-        private TextBox input2;
-        private TextBox input3;
-        private Button calculateButton;
-        private Label resultLabel;
-
-        public CalculatorWindow()
-        {
-            Text = "Okno Kalkulatora";
-
-            Label label1 = new Label() { Text = "Pierwsza liczba:", Location = new System.Drawing.Point(30, 30) };
-            input1 = new TextBox() { Location = new System.Drawing.Point(150, 30) };
-
-            Label label2 = new Label() { Text = "Druga liczba:", Location = new System.Drawing.Point(30, 70) };
-            input2 = new TextBox() { Location = new System.Drawing.Point(150, 70) };
-
-            Label label3 = new Label() { Text = "Trzecia liczba:", Location = new System.Drawing.Point(30, 120) };
-            input3 = new TextBox() { Location = new System.Drawing.Point(150, 110) };
-
-            calculateButton = new Button() { Text = "Oblicz sumę", Location = new System.Drawing.Point(30, 180) };
-            calculateButton.Click += CalculateSum;
-
-            resultLabel = new Label() { Location = new System.Drawing.Point(30, 150), AutoSize = true };
-
-            Controls.Add(label1);
-            Controls.Add(input1);
-            Controls.Add(label2);
-            Controls.Add(input2);
-            Controls.Add(label3);
-            Controls.Add(input3);
-            Controls.Add(calculateButton);
-            Controls.Add(resultLabel);
-        }
-
-       private void CalculateSum(object sender, EventArgs e)
-{
-    if (int.TryParse(input1.Text, out int liczba1) && int.TryParse(input2.Text, out int liczba2))
-    {
-        int suma = liczba1 + liczba2;
-        resultLabel.Text = "Suma liczb to: " + suma;
-    
-        MessageBox.Show("Obliczenia zakończone. Okno zostanie zamknięte.", "Kalkulator");
-
-        this.Close();
+        InitializeComponent();
+        InitializeComboBox(); 
     }
-    else
+
+    private void InitializeComboBox()
     {
-        resultLabel.Text = "Błędne dane wejściowe.";
+      
+        operationComboBox.Items.Add("Dodawanie");
+        operationComboBox.Items.Add("Odejmowanie");
+        operationComboBox.Items.Add("Mnożenie");
+        operationComboBox.Items.Add("Dzielenie");
+        operationComboBox.SelectedIndex = 0; 
+    }
+
+    private async void Calculate(object sender, EventArgs e)
+    {
+        
+        if (int.TryParse(input1.Text, out int liczba1) && int.TryParse(input2.Text, out int liczba2))
+        {
+            int wynik = 0;
+            string operacja = operationComboBox.SelectedItem.ToString();
+
+            switch (operacja)
+            {
+                case "Dodawanie":
+                    wynik = liczba1 + liczba2;
+                    break;
+                case "Odejmowanie":
+                    wynik = liczba1 - liczba2;
+                    break;
+                case "Mnożenie":
+                    wynik = liczba1 * liczba2;
+                    break;
+                case "Dzielenie":
+                    if (liczba2 != 0)
+                    {
+                        wynik = liczba1 / liczba2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie można dzielić przez zero!", "Błąd");
+                        return; 
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Wybierz poprawną operację!", "Błąd");
+                    return; 
+            }
+
+        
+            resultLabel.Text = "Wynik: " + wynik;
+
+        
+            MessageBox.Show("Obliczenia zakończone. Okno zostanie zamknięte za 5 sekund.", "Kalkulator");
+
+            
+
+           
+            this.Close();
+        }
+        else
+        {
+            resultLabel.Text = "Błędne dane wejściowe.";
+        }
     }
 }
 
-        }
-    }
-
-
+    
